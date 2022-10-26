add69d631bba599445d085e7aeaf838dc6e50c8a
Dmitrijs Finaskins <dmitrijs.finaskins@edigard.no>
2022-10-26 21:55:26 +0300

    initial project setup

---
+++ src/main/java/com/example/demo/controller/WeatherForecastController.java
@@ -0,0 +1,9 @@
+package com.example.demo.controller;
+
+public interface WeatherForecastController {
+
+    void getWeatherForecast(String city, String country);
+
+    void getWeatherForecast(String city);
+
+}

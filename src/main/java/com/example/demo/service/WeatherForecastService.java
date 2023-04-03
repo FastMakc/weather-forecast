efe870cdbdd1922cfceaa405f5800d44b87c452f
FastMakc <122231638+FastMakc@users.noreply.github.com>
2023-04-03 23:45:08 +0300

    Update WeatherForecastService.java

--- src/main/java/com/example/demo/service/WeatherForecastService.java
+++ src/main/java/com/example/demo/service/WeatherForecastService.java
@@ -6,7 +6,9 @@ import reactor.core.publisher.Mono;
 
 public interface WeatherForecastService {
 
+
     Mono<RemoteResponseWeather> getForecast(double lat, double lon); // Mono 0..1; Flux 0..n
     Mono<RemoteResponseWeather> getPlace(String city);
     Mono<RemoteResponsePollution> getPollution(double lat, double lon);
+
 }

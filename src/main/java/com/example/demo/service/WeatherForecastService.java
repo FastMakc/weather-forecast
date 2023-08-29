3aca03ffc146b58a6ccb472fd5ccfc8009399cbe
Max Fedorenko <122231638+FastMakc@users.noreply.github.com>
2023-08-29 09:06:43 +0300

    Update WeatherForecastService.java

--- src/main/java/com/example/demo/service/WeatherForecastService.java
+++ src/main/java/com/example/demo/service/WeatherForecastService.java
@@ -10,5 +10,4 @@ public interface WeatherForecastService {
     Mono<RemoteResponseWeather> getForecast(double lat, double lon); // Mono 0..1; Flux 0..n
     Mono<RemoteResponseWeather> getPlace(String city);
     Mono<RemoteResponsePollution> getPollution(double lat, double lon);
-
 }
